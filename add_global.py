03c97bc40431782fcbb7e15db4572b22ac7183ae
Maoziyah <maoziyahlewi@gmail.com>
2023-11-15 13:12:05 -0500

    commit

---
+++ add_global.py
@@ -0,0 +1,12 @@
+Var = int(input("enter varibale"))
+
+def add(number1):
+     return(number1 + Var )
+
+
+def main():
+     number1 = add(10)
+     print ("Sum:", number1 ,"is a whole number")
+
+if __name__ == "__main__":
+     main()
