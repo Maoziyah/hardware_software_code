f9b7ffa833660e7be2ef5e04c6be62ddfe6b844a
Maoziyah <maoziyahlewi@gmail.com>
2023-11-29 12:00:07 -0500

    commit

---
+++ ask_me.py
@@ -0,0 +1,13 @@
+def user_selection():
+     ans = input ("Enter selection:")
+     if ans == "q":
+         return False
+     return True
+
+def main():
+    run_me = True
+    while run_me:
+         run_me = user_selection()
+
+if __name__ == "__main__":
+     main()
