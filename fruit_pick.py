a7817c7adadd33a6ea3bf595dc32f481d64c01bf
Maoziyah <maoziyahlewi@gmail.com>
2023-12-06 12:07:42 -0500

    commit

---
+++ fruit_pick.py
@@ -0,0 +1,19 @@
+def display_menu():
+  menu_dict = {
+    '1':'apples',
+    '2':'bananas',
+    '3':'cherries',
+    'X':'pick your own'
+  }
+  for items, values in menu_dict.items():
+    print(items+". "+ values.capitalize())
+    return menu_dict
+
+def main():
+  menu_dict = display_menu()
+  print(list(menu_dict.items()))
+  print(list(menu_dict))
+  print(list(menu_dict.values()))
+
+if __name__ == "__main__":
+       main()
